5b06983118e4050b9f6e97b75c25c6872b6cd77e
Ramya Gangula <ramya1331@gmail.com>
2019-07-07 15:47:10 -0500

    updated time.sleep to 220 sec

--- application.py
+++ application.py
@@ -20,9 +20,9 @@ def hello():
     Values = ['c','6','c']
     cursor.execute(SQLCommand,Values)
     conn.commit()
-    print("Before sleep statement with 200 sec")
-    time.sleep(200)
-    print("After sleep statement with 200 sec")
+    print("Before sleep statement with 220 sec")
+    time.sleep(220)
+    print("After sleep statement with 220 sec")
     return "Hello World!"    
 
 if __name__ == "__main__":
