ee31138b5879dc1d8b7d28f9906794e3c615f501
Ramya Gangula <ramya1331@gmail.com>
2019-07-08 10:54:28 -0500

    commented sql code

--- application.py
+++ application.py
@@ -9,17 +9,17 @@ password='Password1'
 
 app = Flask(__name__)
 
-conn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password)
-cursor=conn.cursor()
+# conn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password)
+# cursor=conn.cursor()
 
 @app.route("/")
 def hello():
-    SQLCommand = ("INSERT INTO dbo.employee "
-                 "(name, age, place) "
-                 "VALUES (?,?,?)")
-    Values = ['c','6','c']
-    cursor.execute(SQLCommand,Values)
-    conn.commit()
+    # SQLCommand = ("INSERT INTO dbo.employee "
+    #              "(name, age, place) "
+    #              "VALUES (?,?,?)")
+    # Values = ['c','6','c']
+    # cursor.execute(SQLCommand,Values)
+    # conn.commit()
     print("Before sleep statement with 200 sec")
     time.sleep(150)
     print("After sleep statement with 200 sec")
